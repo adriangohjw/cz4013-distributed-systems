c010b68a056edb9df6a9a8722b66c76e5462d8a7
adriangohjw <adriangohjw@gmail.com>
2021-03-26 12:47:44 +0800

    Add Facility.getAvailabilities

--- app/models/Facility.java
+++ app/models/Facility.java
@@ -1,7 +1,9 @@
 package models;
 import java.sql.*;
+import java.util.List;
 
 import databaseServices.Connect;
+import databaseServices.exceptions.RecordNotFoundException;
 
 public class Facility {
 
@@ -35,6 +37,40 @@ public class Facility {
     }
 
   }
+
+
+  public static List<Availability> getAvailabilities(String name, Integer[] daysSelected) {
+    return Availability.getAvailabilitiesForFacility(getIdFromName(name), daysSelected);
+  }
+
+
+  public static Integer getIdFromName(String name) {
+    Integer result = null;
+
+    try {
+      setupConnection();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT id FROM " + tableName + " WHERE name = '" + name + "';");
+      Integer rsCount = 0;
+
+      while (rs.next()) {
+        result = rs.getInt("id");
+        rsCount++;
+      }
+
+      if (rsCount == 0) {
+        throw new RecordNotFoundException();
+      }
+
+      closeConnection(rs, stmt, conn);
+    }
+    catch (Exception e) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage());
+    }
+
+    return result;
+  }
+
 
   private static void setupConnection() {
     try {
