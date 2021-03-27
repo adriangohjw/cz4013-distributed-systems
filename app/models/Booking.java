5de9c2a3b222a3c2520598f1a4551dd2fc379beb
adriangohjw <adriangohjw@gmail.com>
2021-03-27 12:51:11 +0800

    Add constructor for Booking

--- app/models/Booking.java
+++ app/models/Booking.java
@@ -1,5 +1,20 @@
 package models;
+import java.sql.*;
 
-public class Booking {
+import databaseServices.Connect;
+
+public class Booking extends Connect {
+
+  Integer id;
+  Integer facilityId;
+  Timestamp startTime;
+  Timestamp endTime;
+
+  Booking(Integer id, Integer facilityId, Timestamp startTime, Timestamp endTime) {
+    this.id = id;
+    this.facilityId = facilityId;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
 
 }
